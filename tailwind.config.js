6bd78e7fcd644565d57ea933826b4c9cafbe4874
kravening <kajloevesijn@gmail.com>
2023-07-27 16:30:56 +0200

    - fixed some CSS issues (using flex instead of margins to place content in the center lol, yes i know...) - added color randomization button - added color noise scaling parameter
    next up:
    - finalize different noise types for the color noise.
    - implement a drop down that allows you to select the different noise types.
    - make it so the waves configurator gets placed below the text when the screen isn't wide enough to accomodate both side by side (css thingy)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,32 @@ import plugin from 'tailwindcss'
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    strokeColors: {
+      'white': '#ffffff',
+      'black': '#000000',
+    },
+    strokeWidth:{
+      'thin': '1px',
+      'medium': '2px',
+      'large': '3px',
+      'xlarge': '4px',
+    },
     extend: {
       boxShadow: {
         '3xl': '0 0 60px -15px rgba(0, 0, 0, 0.3)',
       },
     },
+    textFillColor: theme => theme('strokeColors.white'),
+    textStrokeColor: theme => theme('strokeColors.black'),
+    textStrokeWidth: theme => theme('strokeWidth.large'),
+    paintOrder: {
+      'fsm': { paintOrder: 'fill stroke markers' },
+      'fms': { paintOrder: 'fill markers stroke' },
+      'sfm': { paintOrder: 'stroke fill markers' },
+      'smf': { paintOrder: 'stroke markers fill' },
+      'mfs': { paintOrder: 'markers fill stroke' },
+      'msf': { paintOrder: 'markers stroke fill' },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-text-fill-stroke'),],
 }
